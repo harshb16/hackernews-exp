e943437bfd611bbbd678aa6e98b304f229d4ef6f
Harsh Bhatia <harshbhatia1605@gmail.com>
2021-05-13 17:22:28 +0530

    Configure colour theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { VStack } from '@chakra-ui/layout';
+import { useColorModeValue, VStack } from '@chakra-ui/react';
 import React from 'react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 
 function App() {
   return (
-    <VStack m={3} p={3} spacing={8}>
-      <ColorModeSwitcher alignSelf='flex-end' />;
+    <VStack
+      minH='100vh'
+      spacing={8}
+      bg={useColorModeValue('blue.300', 'blue.900')}
+    >
+      <ColorModeSwitcher
+        alignSelf='flex-end'
+        m={5}
+        isRound='true'
+        color='gray.500'
+        _hover={{
+          backgroundColor: useColorModeValue('blue.900', 'blue.300'),
+        }}
+      />
+      ;
     </VStack>
   );
 }
