898505ec0050209f95c32d1a181ebe6521f6c394
Harsh Bhatia <harshbhatia1605@gmail.com>
2021-05-13 17:33:00 +0530

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import { useColorModeValue, VStack } from '@chakra-ui/react';
 import React from 'react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
+import Header from './components/Header';
 
 function App() {
   return (
-    <VStack spacing={8}>
+    <VStack>
       <ColorModeSwitcher
         alignSelf='flex-end'
-        m={5}
+        m={3}
         isRound='true'
         color='gray.500'
         _hover={{
           backgroundColor: useColorModeValue('blue.900', 'blue.300'),
         }}
       />
-      ;
+      <Header />
     </VStack>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { Heading } from '@chakra-ui/layout';
+
+const Header = () => {
+  return (
+    <Heading fontWeight='light' color='blue.300' fontSize='4xl'>
+      Hackernews
+    </Heading>
+  );
+};
+
+export default Header;
