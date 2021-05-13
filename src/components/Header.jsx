8a26361b9da595e755193380d088ea1bcb73a226
Harsh Bhatia <harshbhatia1605@gmail.com>
2021-05-13 18:09:56 +0530

    Add gradient to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,12 @@ import { Heading } from '@chakra-ui/layout';
 
 const Header = () => {
   return (
-    <Heading fontWeight='light' color='blue.300' fontSize='4xl'>
+    <Heading
+      fontWeight='light'
+      fontSize={{ base: 'xl', sm: '2xl', lg: '3xl', xl: '5xl' }}
+      bgGradient='linear(to-r, blue.600, blue.400)'
+      bgClip='text'
+    >
       Hackernews
     </Heading>
   );
